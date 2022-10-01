a1a1a5b47e4cfc473d1ff16b08ab0a4ede2ee7f8
Riley Nielsen <rileyjnielsen@gmail.com>
2022-09-30 17:57:54 -0700

    Update argparser.py

--- listenbrainz_file_parser/argparser.py
+++ listenbrainz_file_parser/argparser.py
@@ -7,9 +7,26 @@ def parse_args():
     parser.add_argument(
         "--config",
         help=(
-            "Manually specify config file. "
-            "Default is ~/config_listenbrainz.json "
-        ),
+            "Manually specify config file. Default is ~/config_listenbrainz.json "
+        )
+    )
+    parser.add_argument(
+        "--max-batch",
+        help=(
+            "Maximum number of listens to import per batch"
+        )
+    )
+    parser.add_argument(
+        "--max-total",
+        help=(
+            "Maximum number of listens to import, in total."
+        )
+    )
+    parser.add_argument(
+        "--timeout",
+        help=(
+            "Number of seconds to wait between batches."
+        )
     )
 
     args = parser.parse_args()
